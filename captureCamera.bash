1aae1d6e3cdb80349a38abecf5cb59dafa1d99ba
Muncho <munchou@gmail.com>
2017-01-11 07:56:17 +0000

    capture script fixes

--- captureCamera.bash
+++ captureCamera.bash
@@ -1,25 +1,17 @@
 #!/bin/bash
 
-#fswebcam -d /dev/video0 -r 640x480 --jpeg 85 --greyscale --scale 14x10 -F 5 --no-banner test.jpg
-counter=0
-arr=(0)
-for i in "$@"
-do
-   dir=`dirname $i`
-   filename=`basename $i`
-   newFile=$dir/test$counter.pgm
-   echo -n "Converting $i to $newFile ... "
-   gm convert $i -colorspace GRAY -resize 64x60! -normalize -quality 0 ${newFile}
-   echo -n "sed ... "
-   sed '1,3d' ${newFile} > test.temp
-   echo -n "move ... "
-   mv test.temp ${newFile}
-   arr[$counter]=$newFile
-   ((counter++))
-   rm -f test.temp
-   echo "Done."
-done
-echo "Passing ${arr[*]} to ann ..."
-./ann_main ${arr[*]}
-echo "Removing files: ${arr[*]} ..."
-#rm -f ${arr[*]}
+newFile="test"
+cho "Capture camera to file $newFile ... "
+fswebcam -d /dev/video0 -r 640x480 --jpeg 85 --greyscale --scale 64x60 -F 5 --no-banner ${newFile}.jpg
+echo -n "Converting $newFile ... "
+gm convert ${newFile}.jpg  -colorspace GRAY -resize 64x60! -normalize -quality 0 ${newFile}.pgm
+echo -n "sed ... "
+sed '1,3d' ${newFile}.pgm > test.temp
+echo -n "move ... "
+mv test.temp ${newFile}.pgm
+rm -f test.temp
+echo "Done."
+echo "Passing ${nreFile}.pgm to ann ..."
+./ann_main ${newFile}.pgm
+echo "Removing all of ${newFile} ..."
+#rm -f ${newFile}.pgm ${newFile}.jpg
